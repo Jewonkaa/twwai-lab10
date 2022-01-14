1983250ad9bdd37381edb27836daba44f9879a40
Jewonkaa <80472185+Jewonkaa@users.noreply.github.com>
2022-01-14 19:21:44 +0100

    poprawki

--- dashboard-app/src/components/Data.jsx
+++ dashboard-app/src/components/Data.jsx
@@ -14,7 +14,7 @@ export function Data() {
 
     useEffect(() => {
         const fetchAir = async() => {
-            const res = await fetch("https://twwai-projekt.herokuapp.com/api/params/last");
+            const res = await fetch("https://twwai-lab10.herokuapp.com/api/params/last");
             const dataRes = await res.json();
             chartDataSet(dataRes);
         }
--- dashboard-app/src/components/LineChart.jsx
+++ dashboard-app/src/components/LineChart.jsx
@@ -62,7 +62,7 @@ export function LineChart() {
                ],
            };
 
-           const res = await fetch("http://twwai-projekt.herokuapp.com/api/params")
+           const res = await fetch("http://twwai-lab10.herokuapp.com/api/params")
            const dataRes = await res.json();
 
            data.datasets[0].data = dataRes.map(item => {
